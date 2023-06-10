60502abe00f3276526e26970711e3725db4bece5
Arseny Bulaev <seni190999@mail.ru>
2023-06-10 21:52:06 +0300

    maybe-maybe

--- drift-model/main.cpp
+++ drift-model/main.cpp
@@ -14,8 +14,8 @@ int main() {
 
 	double dz = 0.001;
 	double dt = 0.000001;
-	Well well = Well({ WellSegment(1,0,0,0.1)});
-	MathModel::TaskType task_type = MathModel::TaskType::Debug;
+	Well well = Well({ WellSegment(10,0,0,0.1)});
+	MathModel::TaskType task_type = MathModel::TaskType::BubblesRising;
 	DriftModelSolver solver(dz, dt, well, task_type);
 
 	solver.Solve();
